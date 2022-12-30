231c7a10fcee3f9ce8e603dc609bc7b70342b853
ayushghimiresharma021 <ayushghimiresharma021@gmail.com>
2022-12-30 16:07:34 +0545

    new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Sidebar from './sidebar';
+import Home from './Home';
+import Modal from './modal';
+
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
